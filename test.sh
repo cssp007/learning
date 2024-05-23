eb7d1781cab001fe2e41a04f06c4fc7b260ba32e
Somnath Pandey <44345876+cssp007@users.noreply.github.com>
2024-05-23 21:32:58 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 NAME="Somnath Pandey"
+ARG_1=$1
+ARG_2=$2
 echo "Hello, $NAME. Current time is: $(date)"
 echo "Last job was failed due to typo..Let's check now..!!"
+echo "==============testing using arg===================="
+echo "Value for arg 1 is : $ARG_1",
+echo "Value for arg 2 is : $ARG_2".
